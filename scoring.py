5c0c72a3f305100948117311d7fa579c14c41ea9
Hento42 <hgthomas07@outlook.com>
2026-06-18 23:12:40 +0100

    Began to create the class to store the scores

--- scoring.py
+++ scoring.py
@@ -1,10 +1,30 @@
-def scoring(gameType, maxWickets, maxOvers, wicketRuns, startingRuns, inningsNum, bowlAgain, extraRuns):
+class score(object):
     
-    previousScore = 0
-    previousWickets = 0
-    previousOvers = 0
-    currentScore = 0
-    currentWickets = 0
-    currentOvers = 0.0
+    def __init__(self, gameType, maxWickets, maxOvers, wicketRuns, startingRuns, inningsNum, bowlAgain, extraRuns):
     
-
+        self.runs = 0
+        self.wickets = 0
+        self.overs = 0
+        
+        if gameType == "Fixed Wickets":
+            self.gameType = 0
+        elif gameType == "Fixed Overs":
+            self.gameType = 1
+        elif gameType == "Hundred":
+            self.gameType = 2
+            
+        if bowlAgain == "Yes":
+            self.bowlAgain = 0
+        elif bowlAgain == "No":
+            self.bowlAgain = 1
+        if bowlAgain == "Last Over Only":
+            self.bowlAgain = 2
+            
+        self.maxWickets = maxWickets
+        self.maxOvers = maxOvers
+        self.wicketRuns = wicketRuns
+        self.startingRuns = int(startingRuns)
+        self.inningsNum = int(inningsNum)
+        self.extraRuns = int(extraRuns)
+    
+        
